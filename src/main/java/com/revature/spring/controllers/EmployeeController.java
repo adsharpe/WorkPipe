3f26df9252dfeb719d5a46d8c89e2387aee87c02
Dee Sharpe <demetrioussharpe@aim.com>
2020-03-18 13:20:58 -0400

    Implemented EmployeeController

---
+++ src/main/java/com/revature/spring/controllers/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.revature.spring.controllers;
+
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.hibernate.beans.Employee;
+import com.revature.spring.services.EmployeeService;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+@RequestMapping(value="/employee")
+public class EmployeeController {
+	private Logger log = Logger.getLogger(EmployeeController.class);
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+
+	@GetMapping
+	public ResponseEntity<Set<Employee>> getEmployees(HttpSession session) {
+		Employee employee = (Employee)session.getAttribute("currentUser");
+		log.trace("Following User logged in: " + employee);
+		if(employee == null)
+			return ResponseEntity.status(401).build();
+		
+		log.trace("Getting all employees");
+		
+		return ResponseEntity.ok(employeeService.getEmployees());
+	}
+}
