79e80fe9a4aa7aa5fc798339aff577e1d6f0feb9
Jonathan Mendez <Jonmendez20@gmail.com>
2020-03-19 15:15:14 -0400

    message component

--- src/app/projects/Messages/messages/messages.component.ts
+++ src/app/projects/Messages/messages/messages.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
+import { Text } from 'src/app/shared/classes/text';
+import { TextService } from 'src/app/shared/services/text.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-
-  constructor() { }
+  texts: Text[];
+  text: Text;
+  constructor(
+    private userService: UserService,
+    private textService: TextService,
+  ) { }
 
   ngOnInit(): void {
+   this.text = new Text();
+   this.textService.getTexts().subscribe(
+    (t) => {
+      this.texts = t;
+      this.texts.sort( (t1, t2) => t1.id - t2.id);
+    });
+  }
+
+  isEmployee(): boolean {
+    return this.userService.isEmployee();
   }
 
 }
