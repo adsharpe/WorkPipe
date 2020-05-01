e89f267d8fd0ef29ace1f8ce4d8c5110abf6c965
cdouze <covs.pro@gmail.com>
2020-05-01 12:26:00 -0400

    Updated home pageto only display projects related to the users title. Also, updated task user story. attempted to debug update task story

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, AfterViewInit,ViewEncapsulation, ElementRef, Renderer2 } from '@angular/core';
 import { Project } from './beans/project';
 import { UserService } from '../shared/services/user.service';
 import { ProjectsService } from './services/projects.service';
@@ -11,7 +11,9 @@ import { Employee } from '../shared/classes/employee';
 })
 export class ProjectsComponent implements OnInit {
   public loggedUser: Employee;
+  employee: Employee;
   projects: Project[];
+  empProjects: Project[];
   project: Project;
 
   constructor(
@@ -24,15 +26,19 @@ export class ProjectsComponent implements OnInit {
     //find steps to add imgages to file/folder
     this.renderer.setStyle(this.el.nativeElement.ownerDocument.body,'backgroundImage', "url(' http://www.astmedicalbilling.org/wp-content/uploads/2019/07/undraw_group_chat_trans-1024x700-1.png')");
     this.renderer.setStyle(this.el.nativeElement.ownerDocument.body,'background-repeat', 'no-repeat');
+    this.renderer.setStyle(this.el.nativeElement.ownerDocument.body,'background-attachmen', 'fixed');
     this.renderer.setStyle(this.el.nativeElement.ownerDocument.body,'background-position', 'center');
   }
   ngOnInit(): void {
+    this.employee = this.userService.getEmployee();
+    console.log(this.employee.title)
     this.project = new Project();
     this.projectsService.getProjects().subscribe(
       (p) =>{
         this.projects = p;
         this.projects.sort( (p1, p2) => p1.id - p2.id);
         console.log(p);
+        
       }
     )
   }
